ecdde91032989bd4c0da4be6922045bef565b8d3
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-02-21 08:42:37 +0530

    Time: 1 ms (30.40%), Space: 48 MB (85.53%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum^=nums[i];
+        }
+        return sum;
+    }
+}
