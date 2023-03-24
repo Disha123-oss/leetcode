3391d9c2269976fd9acd613041696fc804bd8f20
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-03-24 12:41:08 +0530

    Time: 653 ms (18.64%), Space: 50.2 MB (40.87%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,29 +1,37 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        int i=0,j=0,k=0;
-		List<Integer> al = new ArrayList<>();
-		List<List<Integer>> list = new ArrayList<>();
-		Arrays.sort(nums);
-		while(i<nums.length-2){
-			j=i+1;
-			k=nums.length-1;
-			while(j<k){
-				if(nums[i]+nums[j]+nums[k]<0)
-					j++;
-				else if(nums[i]+nums[j]+nums[k]>0)
-					k--;
-				else if(nums[i]+nums[j]+nums[k]==0){
-					al.add(nums[i]);
-					al.add(nums[j]);
-					al.add(nums[k]);
-                    if(!list.contains(al))
-                        list.add(al);
-                    al = new ArrayList<>();
-                    j++;
-				}
-			}
-			i++;
-		}
-		return list;
+    public List<List<Integer>> threeSum(int[] arr) {
+         Set<List<Integer>> result  = new HashSet<>();
+        
+        if (arr == null || arr.length < 3)
+            return new ArrayList<>(result);
+       
+        Arrays.sort(arr);									// Sort the elements
+
+        int size = arr.length;
+        
+	    for (int i = 0; i < size - 2; i++)				// Now fix the first element and find the other two elements
+	    {
+	        // To find the other two elements, start two index variables
+	        // from two corners of the array and move them toward each other
+	        int left = i + 1;
+			int right = size - 1;
+	        
+	        while (left < right)
+	        {
+                int sum =  arr[i] + arr[left] + arr[right];
+                
+	            if (sum == 0)
+	            {
+                    result.add(Arrays.asList(arr[i], arr[left], arr[right]));
+                    left++;
+                    right--;
+	            }
+	            else if (sum < 0)
+	                left++;
+	            else 													
+	                right--;
+	        }
+	    }
+        return new ArrayList<>(result);
     }
 }
