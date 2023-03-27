da915ef4d32cdb949251a2ef07c56a18363d3d6a
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-03-27 14:31:41 +0530

    Time: 270 ms (8.58%), Space: 43.6 MB (23.28%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,38 +1,42 @@
 class Solution {
+    public boolean check(HashMap<Character,Integer> hm1, HashMap<Character,Integer> hm2){
+        for(char ch : hm2.keySet()){
+            if(hm1.containsKey(ch)){
+                if(hm1.get(ch) < hm2.get(ch))
+                    return false;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
     public String minWindow(String s, String t) {
-        int st=0,e=0,count=0, start=0,end=0, ans=Integer.MAX_VALUE, distinct=0;
-		int[] tfre = new int[123];
-		for(int i=0;i<t.length();i++){
-			tfre[t.charAt(i)]++;
-			if(tfre[t.charAt(i)] == 1)
-				distinct++;
-		}
-		int[] sfre = new int[123];
-		
-		while(e<s.length()){
-			//expansion
-			sfre[s.charAt(e)]++;
-			if(sfre[s.charAt(e)] == tfre[s.charAt(e)])
-				count++;
-			e++;
-
-			//contraction
-			while(st<e && count == distinct){
-				if(ans>e-st){
-                    ans = e-st;
-                    start = st;
-                    end = e;
+        int st=0,e=0,n=s.length(),min=Integer.MAX_VALUE,start=-1,end=-1;
+        HashMap<Character,Integer> hm1 = new HashMap<>();
+        HashMap<Character,Integer> hm2 = new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            hm2.put(t.charAt(i),hm2.getOrDefault(t.charAt(i),0)+1);
+        }
+        while(e<n){
+            //expansion
+            hm1.put(s.charAt(e),hm1.getOrDefault(s.charAt(e),0)+1);
+            e++;
+            
+            //contraction
+            while(check(hm1,hm2)){
+                if(min>(e-st)){
+                    min=Math.min(min,e-st);
+                    start=st;
+                    end=e;
                 }
-				if(sfre[s.charAt(st)] == tfre[s.charAt(st)])
-					count--;
-				sfre[s.charAt(st)]--;
-				st++;
-			}
-		}
-		String str = "";
-		str += s.substring(start,end);
-		if(ans==Integer.MAX_VALUE)
-			return "";
-		return str;
+                if(hm1.getOrDefault(s.charAt(st),0)==1)
+                    hm1.remove(s.charAt(st));
+                else
+                    hm1.put(s.charAt(st),hm1.getOrDefault(s.charAt(st),0)-1);
+                st++;
+            }
+        }
+        if(start==-1 && end==-1)   return "";
+        return s.substring(start,end);
     }
 }
