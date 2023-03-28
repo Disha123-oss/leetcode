4cb8bd29ef71376f6d7db70d9662c69b6eb94bdc
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-03-28 22:55:37 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (14.21%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -14,14 +14,10 @@
  * }
  */
 class Solution {
-   
     public int maxDepth(TreeNode root) {
-        if(root==null)
-            return 0;
-        
-        int lh = maxDepth(root.left);
-        int rh = maxDepth(root.right);
-        
-        return Math.max(lh,rh)+1;
+        if(root==null)  return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1 + Math.max(left,right);
     }
 }
