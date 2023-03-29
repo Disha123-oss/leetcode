6a6c56b50a3c7900a91db7cf17ace2a237de087f
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-03-29 20:13:00 +0530

    Time: 11 ms (11.11%), Space: 43.7 MB (63.58%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,37 +8,31 @@
  * }
  */
 class Solution {
-    Stack<TreeNode> st = new Stack<>();
-    public ArrayList<TreeNode> lca(TreeNode root, TreeNode p, TreeNode q){
-        if(root==null){
-            ArrayList<TreeNode> base = new ArrayList<>();
-            return base;
-        }
-        boolean foundp=false, foundq=false;
-        ArrayList<TreeNode> al = new ArrayList<>();
-        ArrayList<TreeNode> left = lca(root.left,p,q);
-        ArrayList<TreeNode> right = lca(root.right,p,q);
-        for(int i=0;i<left.size();i++)
-            al.add(left.get(i));
-        for(int i=0;i<right.size();i++)
-            al.add(right.get(i));
+    public void path(TreeNode root, int target, List<TreeNode> al, List<TreeNode> res){
+        if(root==null)  return;
         al.add(root);
-        for(int i=0;i<al.size();i++){
-            if(al.get(i).val==p.val)
-                foundp=true;
-            if(al.get(i).val==q.val)
-                foundq=true;
+        if(root.val==target){
+            for(int i=0;i<al.size();i++){
+                res.add(al.get(i));
+            }
         }
-        if(foundp==true && foundq==true){
-            if(st.size()==0)
-                st.push(al.get(al.size()-1));
-            return al;
-        }
-        return al;
+        path(root.left,target,al,res);
+        path(root.right,target,al,res);
+        al.remove(al.size()-1);
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> al = new ArrayList<>();
-        ArrayList<TreeNode> list = lca(root,p,q);
-        return st.pop();
+        List<TreeNode> al = new ArrayList<>();
+        List<TreeNode> res1 = new ArrayList<>();
+        path(root,p.val,al,res1);
+        al = new ArrayList<>();
+        List<TreeNode> res2 = new ArrayList<>();
+        path(root,q.val,al,res2);
+        int min = Math.min(res1.size(),res2.size());
+        TreeNode ans=null;
+        for(int i=0;i<min;i++){
+            if(res1.get(i)==res2.get(i))
+                ans=res1.get(i);
+        }
+        return ans;
     }
 }
