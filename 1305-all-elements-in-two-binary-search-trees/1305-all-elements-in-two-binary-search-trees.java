6dcf0d26a2f46f5398bce96a0784f0c6d017b281
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-04-14 21:20:48 +0530

    Time: 24 ms (30.16%), Space: 45.8 MB (21.46%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -14,17 +14,39 @@
  * }
  */
 class Solution {
-    List<Integer> al = new ArrayList<>();
-    public void inorder(TreeNode root){
+   
+    public void inorder(TreeNode root, List<Integer> al){
         if(root==null)  return;
-        inorder(root.left);
+        inorder(root.left,al);
         al.add(root.val);
-        inorder(root.right);
+        inorder(root.right,al);
     }
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        inorder(root1);
-        inorder(root2);
-        Collections.sort(al);
-        return al;
+        List<Integer> al1 = new ArrayList<>();
+        List<Integer> al2 = new ArrayList<>();
+        List<Integer> al3 = new ArrayList<>();
+        inorder(root1,al1);
+        inorder(root2,al2);
+        //merge two sorted lists
+        int i=0,j=0;
+        while(i<al1.size() && j<al2.size()){
+            if(al1.get(i)<=al2.get(j)){
+                al3.add(al1.get(i));
+                i++;
+            }
+            else if(al1.get(i)>al2.get(j)){
+                al3.add(al2.get(j));
+                j++;
+            }
+        }
+        while(i<al1.size()){
+            al3.add(al1.get(i));
+            i++;
+        }
+        while(j<al2.size()){
+            al3.add(al2.get(j));
+            j++;
+        }
+        return al3;
     }
 }
