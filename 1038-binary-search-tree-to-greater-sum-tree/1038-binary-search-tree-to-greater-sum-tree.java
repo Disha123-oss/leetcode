db5789b5a642547a222b2fa0a4cebefd46ea727b
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-04-14 08:40:55 +0530

    Time: 0 ms (100.00%), Space: 40 MB (68.27%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -14,16 +14,16 @@
  * }
  */
 class Solution {
-    int sum = 0;
-    public void inorder(TreeNode root){
-        if(root==null)   return;
-        inorder(root.right);
-        root.val+=sum;
-        sum=root.val;
-        inorder(root.left);
+    int sum=0;
+    public void gst(TreeNode root){
+        if(root==null)  return;
+        gst(root.right);
+        sum+=root.val;
+        root.val=sum;
+        gst(root.left);
     }
     public TreeNode bstToGst(TreeNode root) {
-        inorder(root);
+        gst(root);
         return root;
     }
 }
