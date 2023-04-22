85bb959a428d491504c01705a90d6fe454b83f16
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-04-22 11:40:44 +0530

    Time: 88 ms (27.09%), Space: 65.6 MB (100.00%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
@@ -24,8 +24,8 @@ class Pair{
 class Solution {
     public TreeNode replaceValueInTree(TreeNode root) {
         Queue<Pair> q = new LinkedList<>();
-        List<Integer> al = new ArrayList<>();
         q.add(new Pair(root,null));
+        Map<TreeNode,Integer> hm = new HashMap<>();
         while(q.size()>0){
             int size = q.size();
             List<Pair> list  = new ArrayList<>();
@@ -34,38 +34,43 @@ class Solution {
                 Pair nn = q.remove();
                 TreeNode temp = nn.node;
                 TreeNode parent = nn.parent;
+                int val=0;
                 list.add(new Pair(temp,parent));
                 sum+=temp.val;
-                if(temp.left!=null) q.add(new Pair(temp.left,temp));
-                if(temp.right!=null) q.add(new Pair(temp.right,temp));
+                if(temp.left!=null){
+                    q.add(new Pair(temp.left,temp));
+                    val+=temp.left.val;
+                }
+                if(temp.right!=null){
+                    q.add(new Pair(temp.right,temp));
+                    val+=temp.right.val;
+                }
+                hm.put(temp,val);
             }
             for(Pair p : list){
                 TreeNode node = p.node;
                 TreeNode parent = p.parent;
-                int sub = 0;
                 if(parent==null){
                     node.val=0;
                     continue;
                 }
-                if(parent.left!=null) sub+=parent.left.val;
-                if(parent.right!=null) sub+=parent.right.val;
-                al.add(sum-sub);
+                node.val = sum-hm.get(parent);
             }
         }
-        Queue<TreeNode> que = new LinkedList<>();
-        que.add(root);
-        int j=0;
-        System.out.print(al.size());
-        while(que.size()>0){
-            int size = que.size();
-            for(int i=0;i<size;i++){
-                TreeNode temp = que.remove();
-                if(temp!=root)
-                    temp.val = al.get(j++);
-                if(temp.left!=null) que.add(temp.left);
-                if(temp.right!=null) que.add(temp.right);
-            }
-        }
+        // Queue<TreeNode> que = new LinkedList<>();
+        // que.add(root);
+        // int j=0;
+        // System.out.print(al.size());
+        // while(que.size()>0){
+        //     int size = que.size();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode temp = que.remove();
+        //         if(temp!=root)
+        //             temp.val = al.get(j++);
+        //         if(temp.left!=null) que.add(temp.left);
+        //         if(temp.right!=null) que.add(temp.right);
+        //     }
+        // }
         return root;
     }
 }
