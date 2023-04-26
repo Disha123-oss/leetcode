6dd8a2ddb7678c7c93fc3f7f8b7162788d2894ea
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-04-26 10:03:07 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (11.73%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,11 +1,19 @@
 class Solution {
-    public int numTrees(int n) {
+    public int solve(int n, int[] dp) {
         if(n<=1)
             return 1;
+        if(dp[n]!=-1)
+            return dp[n];
         int sum=0;
         for(int i=1;i<=n;i++){
-            sum+=numTrees(i-1)*numTrees(n-i);
+            sum+=solve(i-1,dp)*solve(n-i,dp);
         }
+        dp[n]=sum;
         return sum;
+    }
+    public int numTrees(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
     }
 }
