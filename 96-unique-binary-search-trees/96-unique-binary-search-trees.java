3b7a1926db9150ae2a3428251781bbb9572e206b
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-04-26 09:57:31 +0530

    Time: 2266 ms (7.46%), Space: 39.7 MB (20.06%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numTrees(int n) {
+        if(n<=1)
+            return 1;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=numTrees(i-1)*numTrees(n-i);
+        }
+        return sum;
+    }
+}
