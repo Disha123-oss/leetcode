316487334b13f0757cb3a0aaaa04a04c74e15a7f
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-05-01 08:21:15 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (53.15%) - LeetHub

--- 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
@@ -16,20 +16,19 @@
 class Solution {
     int lc=0,rc=0;
     public int count(TreeNode root, int x){
-        if(root==null)
-            return 0;
+        if(root==null) return 0;
         int left = count(root.left,x);
         int right = count(root.right,x);
         if(root.val==x){
-            lc=left;
-            rc=right;
+            lc = left;
+            rc = right;
         }
         return left+right+1;
-    }
+    } 
     public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
         count(root,x);
-        int parent = n - (lc+rc+1);
+        int parent = n-lc-rc-1;
         int max = Math.max(parent,Math.max(lc,rc));
-        return max > n-max;
+        return max>(n/2);
     }
 }
