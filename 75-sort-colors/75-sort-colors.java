0a283f6e92abe06b36c0a91f784938a3e17e4ca5
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-05-30 12:58:43 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (14.94%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,11 +1,12 @@
 class Solution {
     public void swap(int[] nums, int i, int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     public void sortColors(int[] nums) {
-        int p1=-1,p2=0,p3=nums.length-1;
+        int n = nums.length;
+        int p1=-1, p2=0, p3=n-1;  //p2 to p3 is the undiscovered area
         while(p2<=p3){
             if(nums[p2]==2){
                 swap(nums,p2,p3);
