8d005fa3aa71a0d65d718a774ac664e8c2460fdf
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-06-03 11:06:27 +0530

    Time: 49 ms (53.00%), Space: 52 MB (5.19%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,33 +1,28 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] arr) {
-         Set<List<Integer>> result  = new HashSet<>();
-       
-        Arrays.sort(arr);									// Sort the elements
-
-        int size = arr.length;
-        
-	    for (int i = 0; i < size - 2; i++)				// Now fix the first element and find the other two elements
-	    {
-	        // To find the other two elements, start two index variables
-	        // from two corners of the array and move them toward each other
-	        int left = i + 1;
-			int right = size - 1;
-	        
-	        while (left < right)
-	        {
-                int sum =  arr[i] + arr[left] + arr[right];
-                
-	            if (sum == 0)
-	            {
-                    result.add(Arrays.asList(arr[i], arr[left], arr[right]));
-                    left++;
-	            }
-	            else if (sum < 0)
-	                left++;
-	            else 													
-	                right--;
-	        }
-	    }
-        return new ArrayList<>(result);
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0;i<n-2;i++){
+            while(i>0 && i<n-2 && nums[i]==nums[i-1]) i++;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    ArrayList<Integer> al = new ArrayList<>();
+                    al.addAll(Arrays.asList(nums[i],nums[j],nums[k]));
+                    list.add(al);
+                    j++; k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+                else if(sum<0)
+                    j++;
+                else if(sum>0)
+                    k--;
+            }
+        }
+        return list;
     }
 }
