5773d98c3014973f0bdda4ce5f494762e7aac57f
Swagata Dutta <65579456+Disha123-oss@users.noreply.github.com>
2023-06-12 13:31:53 +0530

    Added solution - LeetHub

---
+++ Ceil The Floor - GFG/ceil-the-floor.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Pair ans = new Solve().getFloorAndCeil(arr, n, x);
+            System.out.println(ans.floor + " " + ans.ceil);
+        }
+    }
+}
+
+class Pair {
+    int floor, ceil;
+
+    Pair() {
+        this.floor = 0;
+        this.ceil = 0;
+    }
+
+    Pair(int floor, int ceil) {
+        this.floor = floor;
+        this.ceil = ceil;
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solve {
+    public int ceil(int[] arr, int n, int x){
+        int low=0, high=n-1, ceil=-1;
+        while(low<=high){
+            int mid =(low+high)/2;
+            if(arr[mid]==x) return arr[mid];
+            else if(arr[mid]>x){
+                ceil=arr[mid];
+                high=mid-1;
+            }
+            else
+                low=mid+1;
+        }
+        return ceil;
+    }
+    public int floor(int[] arr, int n, int x){
+        int low=0, high=n-1, floor=-1;
+        while(low<=high){
+            int mid =(low+high)/2;
+            if(arr[mid]==x) return arr[mid];
+            else if(arr[mid]<x){
+                floor=arr[mid];
+                low=mid+1;
+            }
+            else
+                high=mid-1;
+        }
+        return floor;
+    }
+    Pair getFloorAndCeil(int[] arr, int n, int x) {
+        // code here
+        Arrays.sort(arr);
+        int f = floor(arr,n,x);
+        int c = ceil(arr,n,x);
+        return new Pair(f,c);
+    }
+}
+
